449ae04ec27e7209b7dd07d5f1b780f7e698a358
xuxiaoxin <xiaoxinxu@126.com>
2015-06-15 18:02:31 +0800

    init commit

---
+++ genstring.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# GPLv2
+
+"""
+将base语言资源文件合并到其他语言
+"""
+
+import sys, os, localizable
+
+format_encoding = 'UTF-8'
+
+class GenString:
+    def __init__(self, path, base, dest):
+        self.path = path
+        self.base = base
+        self.dest = dest
+        pass
+    
+    def gen(self):
+        basePath = os.path.join(self.path, self.base + '.lproj/Main.strings')
+        destPath = os.path.join(self.path, self.dest + '.lproj/Main.strings')
+        outPath  = os.path.join(self.path, self.dest + '.lproj/Main.out.strings')
+        
+        print 'Loading strings files ...'
+        print basePath
+        baseLang = localizable.parse_strings(filename = basePath)
+        print destPath
+        destLang = localizable.parse_strings(filename = destPath)
+        print 'Done'
+        #print baseLang
+        #print destLang
+        
+        print 'Merging ...',
+        
+        # 将 base 中新增的 merge 到 destLang
+        merged = self._mergeTo(baseLang, destLang)
+        
+        print 'Done'
+        self._generate_strings_file(outPath, merged)
+        pass
+        
+        
+
+    # 从 src 集合向 dest 集合 做 merge
+    def _mergeTo(self, src, dest):
+        out = []
+        for item in src:
+            destItem = self._find_in(item['key'], dest)
+            if(destItem is None):
+                out.append(item)
+            else:
+                out.append(destItem)
+        return out
+                
+    # 从 src 中查找，并返回第一个            
+    def _find_in(self, key, src):
+        for item in src:
+            if(key == item['key']):
+                return item
+        return None
+        
+    
+    # 生成新的 strings 文件
+    def _generate_strings_file(self, filePath, out):
+        #    /* Class = "UILabel"; text = "mail"; ObjectID = "0Ew-k9-4Yr"; */
+        #    "0Ew-k9-4Yr.text" = "mail";
+        
+        fw = open(filePath, 'w+')
+        for item in out:
+            content = u'\n/*' + item['comment'] + u'*/\n' + u'"' + item['key'] + u'" = "' + item['value'] + u'"\n'
+            fw.write(content.encode(format_encoding))
+        fw.close();
+        return
+        
+    # end of class GenString
+    pass
+
+
+
+def usage():
+    print "Usage: "+sys.argv[0]+" <ProjectPath> <BaseLang> <DestinationLang>"
+    print '*'*40
+    print "      ProjectPath XCode Project path"
+    print "      BaseLang name aka. en"
+    print "      Destination Lang name aka. zh-Hans"
+    print '*'*40
+    sys.exit(0)
+    pass
+    
+def main():
+    
+    if len(sys.argv) != 4:
+        usage()
+        
+    project_path = sys.argv[1]
+    base_lang = sys.argv[2]
+    dest_lang = sys.argv[3]
+    g = GenString(project_path, base_lang, dest_lang);
+    g.gen()
+
+    pass
+
+if __name__ == '__main__':
+    main()
